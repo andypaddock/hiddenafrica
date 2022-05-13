1509eed56e08fb9923d9f38719616c3fbf432c40
Andy Paddock <appaddock@gmail.com>
2022-05-13 10:40:50 +0100

    form

--- template-parts/logo_slider.php
+++ template-parts/logo_slider.php
@@ -19,6 +19,7 @@
                 <?php 
 $link = get_sub_field('link','options');
 $partLogo = get_sub_field('logo','options');
+if ($link):
     $link_url = $link['url'];
     $link_title = $link['title'];
     $link_target = $link['target'] ? $link['target'] : '_self';
@@ -26,6 +27,10 @@ $partLogo = get_sub_field('logo','options');
                 <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><img
                         src="<?php echo esc_url($partLogo['url']); ?>"
                         alt="<?php echo esc_attr($partLogo['alt']); ?>" /></a>
+
+                <?php else: ?>
+                <img src="<?php echo esc_url($partLogo['url']); ?>" alt="<?php echo esc_attr($partLogo['alt']); ?>" />
+                <?php endif; ?>
             </div>
             <?php endwhile; ?>
             <?php endif; ?>
